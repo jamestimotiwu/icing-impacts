8af0d75ed47ffc0e0a2a8e6f09b83160036375d6
James <james.timotiwu@nasa.gov>
2017-06-28 11:51:54 -0400

    Reorganized repository

---
+++ Segmentation.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+from pandas import DataFrame, read_csv
+
+runNumber
+thresholdUsed
+connectivity
+res
+framerate
+DT
+P
+outputdata
+outputdata2
+histogram
+frame
+movieframes
+
+OutputTxt01
+OutputTxt02
+
+WedgeAngle
+
+#Read Image
+
+#Crop Image [1 1 6250 4380]
+
+
+
+
