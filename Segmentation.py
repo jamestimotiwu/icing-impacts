86dc29d03fc5f7acf946f32199300768baafbc05
James <james.timotiwu@nasa.gov>
2017-07-06 15:11:20 -0400

    Working Demonstration Jupyter Notebook

--- Segmentation.py
+++ Segmentation.py
@@ -33,7 +33,7 @@ from skimage.filters import threshold_otsu
 
 #Main definition for program
 def main():
-    io.use_plugin('tifffile')
+
 
     cropped = openimage(os.path.join(os.getcwd(), 'data', '5 psi Runs', '5psi_1.tiff'))
    #write cropped
@@ -46,7 +46,7 @@ def main():
     p1, p2 = np.percentile(cropped, (97, 99.994))
     Iadj = expo.rescale_intensity(cropped, in_range=(p1,p2))
 #    Iadj = expo.rescale_intensity(cropped)
-    I8 = img_as_ubyte(cropped)
+#    I8 = img_as_ubyte(cropped)
 #imread Baseline.tif, Crop, Adjust Brightness, write
 
     bgimg = openimage(os.path.join(os.getcwd(), 'data', '5 psi Runs', '5psi baseline.tiff')) 
@@ -54,15 +54,17 @@ def main():
     p3, p4 = np.percentile(bgimg, (0,100))
     bgadj = expo.rescale_intensity(bgimg, in_range = (p1,p2))
 
-    b8 = img_as_ubyte(bgimg)
+#    b8 = img_as_ubyte(bgimg)
 
-    io.imsave(os.path.join(os.getcwd(), 'data', '5 psi Runs', '5psi_ADJ.tif'),
-            I8)
-    io.imsave(os.path.join(os.getcwd(), 'data', '5 psi Runs', 'BG_ADJ.tif'), b8)
+#    io.imsave(os.path.join(os.getcwd(), 'data', '5 psi Runs', '5psi_ADJ.tif'),
+
+#    io.imsave(os.path.join(os.getcwd(), 'data', '5 psi Runs', 'BG_ADJ.tif'), b8)
 #    B8 = img_as_ubyte(bgadj)
 #subtract I8 and Background imsubtract(I, Background), convert to I8, write
 #imwrite(I8, '05-I8 After Crop-ImAdj-BkgndSubt.tif', 'compression', 'none')
-    isubt = Iadj - bgadj
+#    isubt = Iadj - bgadj
+    isubt = cropped - bgimg 
+    print(isubt)
 #    isubt = ropped - bgimg
 #InvertIm(I) to get inverted image, convert to I8, write as '06-I8 After Cro
 #-ImAdj-BkgndSubt-Invert.tif'
@@ -75,7 +77,7 @@ def main():
 #DO segmentation, I = im2bw(I, level<- threshold used), imwrite as '07-I After
 #Crop-ImAdj-BkgndSubt-Thresh.tif'
     threshed = threshold_otsu(isubt)
-
+    print(threshed)
 #    threshed8 = img_as_ubyte(threshed) 
 #Use I2 as Ior, invert final segmented I and write as '08-bwf Ready for
 #Segmentation.tif'
@@ -92,18 +94,18 @@ def main():
     ax[1] = plt.subplot(1, 3, 2, sharex=ax[0], sharey=ax[0], adjustable='box-forced')
     ax[2] = plt.subplot(1, 3, 3, sharex=ax[0], sharey=ax[0], adjustable='box-forced')
     
-    ax[0].imshow(I8, cmap=plt.cm.gray)
+    ax[0].imshow(Iadj)#, cmap=plt.cm.gray)
     ax[0].set_title('Original')
     ax[0].axis('off')
 
-    ax[1].imshow(bgimg, cmap=plt.cm.gray)
-    ax[1].set_title('Original')
+    ax[1].imshow(isubt, cmap=plt.cm.gray)
+    ax[1].set_title('Original/Adjusted')
     ax[1].axis('off')
 #    ax[1].hist(isubt.ravel(), bins=40)
 #    ax[1].set_title('Histogram')
 #    ax[1].axvline(threshed, color='r')
     
-    ax[2].imshow(binaryinverted,cmap=plt.cm.gray)
+    ax[2].imshow(binary,cmap=plt.cm.gray)
     ax[2].set_title('Thresholded')
     ax[2].axis('off')
 
@@ -113,8 +115,7 @@ def main():
 
 #Read Image, Crop, Convert to I8
 def openimage(image):
-    io.use_plugin('tifffile')
-    I8 = io.imread(image, plugin='tifffile')
+    I8 = io.imread(image).astype(np.float32)
 #    I8 = img_as_ubyte(io.imread(image)) #Image as UINT8
     return I8[1:4380, 1:6250] #Crop Image [1 1 6250 4380]
     #imwrite(I8, '01-18 Original_Cropped.tif', 'compression', 'none')
